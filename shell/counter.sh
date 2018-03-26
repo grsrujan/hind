05fc20454943867deb807b76975796bd8904355b
grsrujan <srujansid@gmail.com>
2018-03-26 13:01:58 -0400

    Update counter.sh

--- shell/counter.sh
+++ shell/counter.sh
@@ -2,8 +2,8 @@
 counter=0
 while true
   do
-    if [[ condition ]];then
-        echo "number of files in current directory is greater than "
+    if [[ `ls -1 | wc -l` -gt 2 ]];then
+        echo "number of files in current directory is greater than 2"
         break
     else
         counter=$((counter+1))
