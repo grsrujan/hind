0cd020ae8869ebd87b17d2d16e2a698550d03d66
grsrujan <srujansid@gmail.com>
2018-07-17 13:30:37 -0400

    Update ArrayListExample.java

--- java/collections/add/ArrayListExample.java
+++ java/collections/add/ArrayListExample.java
@@ -1,1 +1,23 @@
+package add;
+import java.util.*;
 
+public class ArrayListExample {
+
+	public static void main(String[] args) {
+		
+	// Initialize an arrayList using Arrays.asList
+		ArrayList<String> al = new ArrayList<String>(
+				Arrays.asList("a", "b", "c","d","e","f"));
+			  System.out.println("Elements are:"+al);
+			  
+	// Initialize an arrayList using add
+		ArrayList<String> bl = new ArrayList<String>();
+		
+		bl.add("a");
+		bl.add("b");
+		bl.add("c");
+		bl.add("d");
+		System.out.println("Elements are:"+bl);
+
+	      }	
+	}
