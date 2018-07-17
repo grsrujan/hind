b6981ad1c24a30446cd1da897c81beed25c580c8
grsrujan <srujansid@gmail.com>
2018-07-17 14:05:32 -0400

    Update ArrayListExample.java

--- java/collections/add/ArrayListExample.java
+++ java/collections/add/ArrayListExample.java
@@ -18,6 +18,37 @@ public class ArrayListExample {
 		bl.add("c");
 		bl.add("d");
 		System.out.println("Elements are:"+bl);
+		
+	// add element at particular index
+		al.add(0, "z");
+		System.out.println("Elements are:"+al);
+		
+	// add one list to other
+		
+		al.addAll(bl);
+		System.out.println("Elements are:"+al);
+		
+	// copy one list to other
+		ArrayList<String> cl = new ArrayList<String>();
+		cl.addAll(bl);
+		System.out.println("Elements are:"+cl);
+	
+	// adding list starting from specified location 
+		
+		al.addAll(3, bl);
+		System.out.println("Elements are:"+al);
 
 	      }	
 	}
+
+/*
+
+output:
+Elements are:[a, b, c, d, e, f]
+Elements are:[a, b, c, d]
+Elements are:[z, a, b, c, d, e, f]
+Elements are:[z, a, b, c, d, e, f, a, b, c, d]
+Elements are:[a, b, c, d]
+Elements are:[z, a, b, a, b, c, d, c, d, e, f, a, b, c, d]
+
+*/
