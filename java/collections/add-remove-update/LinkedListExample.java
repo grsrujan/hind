e287bfc77eb113d441b30f89b5beb6ea8eda041c
grsrujan <srujansid@gmail.com>
2018-07-17 16:07:12 -0400

    Update LinkedListExample.java

--- java/collections/add-remove-update/LinkedListExample.java
+++ java/collections/add-remove-update/LinkedListExample.java
@@ -32,13 +32,45 @@ public class LinkedListExample {
 	     
 	    ll.addLast("x");
 	     System.out.println("LinkedList after adding element: "+ll);
-		
+	     
 	     LinkedList<String> la = new LinkedList<String>();
 	     
 	     //copy one list to other
 	     
 	     la.addAll(ll);
 	     System.out.println(la);
+	     
+			System.out.println("LinkedList before remove:"+ll);
+			
+			// Removing First element
+		    Object firstElement = ll.removeFirst();
+		    
+		    System.out.println("LinkedList after remove:"+ll);
+		    
+		    Object lastElement = ll.removeLast();
+		    
+		    System.out.println("LinkedList after remove:"+ll);
+			
+			// remove using index
+			
+			ll.remove(0);
+			ll.remove(1);
+			ll.remove(1);
+			
+			System.out.println("LinkedList after remove:"+ll);
+			
+			// remove using values
+			
+			ll.remove("b");
+			ll.remove("h");
+			
+			System.out.println("LinkedList after remove:"+ll);
+			
+			// remove everything from array list
+			
+			ll.clear();
+			
+			System.out.println("LinkedList after removing everything :"+ll);
 
 	}
 
@@ -52,4 +84,11 @@ LinkedList after adding element: [a, b, c, z, d, e, f, g, a]
 LinkedList after adding element: [d, a, b, c, z, d, e, f, g, a]
 LinkedList after adding element: [d, a, b, c, z, d, e, f, g, a, x]
 [d, a, b, c, z, d, e, f, g, a, x]
+LinkedList before remove:[d, a, b, c, z, d, e, f, g, a, x]
+LinkedList after remove:[a, b, c, z, d, e, f, g, a, x]
+LinkedList after remove:[a, b, c, z, d, e, f, g, a]
+LinkedList after remove:[b, d, e, f, g, a]
+LinkedList after remove:[d, e, f, g, a]
+LinkedList after removing everything :[]
+
 */
