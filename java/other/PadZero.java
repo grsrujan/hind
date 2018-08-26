34af7ad7abc228043853f2716cdb75007db714b4
grsrujan <srujansid@gmail.com>
2018-08-26 00:09:03 -0400

    Update and rename LPadZero.java to PadZero.java

--- java/other/PadZero.java
+++ java/other/PadZero.java
@@ -1,16 +1,17 @@
 package search;
 import org.apache.commons.lang.StringUtils;
 
-public class LPadZero {
+public class PadZero {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 		String a = "222212";		
 		System.out.println(StringUtils.leftPad(a.trim(), 10, "0"));
+		System.out.println(StringUtils.rightPad(a.trim(), 10, "0"));
 
 	}
-
 }
 
 // output: 0000222212
+// 2222120000
