e6bca8adbad00681557edd5992128e43e1c9fe00
grsrujan <srujansid@gmail.com>
2018-08-25 23:08:40 -0400

    Update and rename a to DisplayDate.java

---
+++ java/other/DisplayDate.java
@@ -0,0 +1,32 @@
+package search;
+import java.util.Date;
+
+import org.apache.commons.lang.StringUtils;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+public class Ddate {
+
+	public static void main(String[] args) throws ParseException{
+		// TODO Auto-generated method stub
+		
+		
+		Date c = new SimpleDateFormat("yyyy-MM-dd").parse("2018-05-05");
+		SimpleDateFormat dt1 = new SimpleDateFormat("yyyy-MM-dd");
+		
+    	System.out.println(c);    	
+    	System.out.println(dt1.format(c));
+    	
+    	String inputDate = "2018142";
+    	String inputTime= "121212";    	
+    	
+  	      StringBuilder sb = new StringBuilder();
+	      sb.append(inputDate.trim()).append(' ').append(StringUtils.leftPad(inputTime.trim(), 7, "0"));
+	      System.out.println(sb.toString());
+		  Date a   = new SimpleDateFormat("yyyyD HHmmssS").parse(sb.toString());
+		  String b = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss:S").format(a);
+		  System.out.println(b); 
+	}
+
+}
