c11664c193651de745d1056c5443dab1d894d30b
grsrujan <srujansid@gmail.com>
2018-08-25 23:09:26 -0400

    Update DisplayDate.java

--- java/other/DisplayDate.java
+++ java/other/DisplayDate.java
@@ -13,18 +13,18 @@ public class Ddate {
 		Date c = new SimpleDateFormat("yyyy-MM-dd").parse("2018-05-05");
 		SimpleDateFormat dt1 = new SimpleDateFormat("yyyy-MM-dd");
 		
-    	System.out.println(c);    	
-    	System.out.println(dt1.format(c));
+    		System.out.println(c);    	
+    		System.out.println(dt1.format(c));
     	
-    	String inputDate = "2018142";
-    	String inputTime= "121212";    	
+    		String inputDate = "2018142";
+    		String inputTime= "121212";    	
     	
-  	      StringBuilder sb = new StringBuilder();
-	      sb.append(inputDate.trim()).append(' ').append(StringUtils.leftPad(inputTime.trim(), 7, "0"));
-	      System.out.println(sb.toString());
-		  Date a   = new SimpleDateFormat("yyyyD HHmmssS").parse(sb.toString());
-		  String b = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss:S").format(a);
-		  System.out.println(b); 
+  	        StringBuilder sb = new StringBuilder();
+	        sb.append(inputDate.trim()).append(' ').append(StringUtils.leftPad(inputTime.trim(), 7, "0"));
+	        System.out.println(sb.toString());
+		Date a   = new SimpleDateFormat("yyyyD HHmmssS").parse(sb.toString());
+		String b = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss:S").format(a);
+		System.out.println(b); 
 	}
 
 }
