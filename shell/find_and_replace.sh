c55fff68bad2ebf92d46fa1edb318b3a6a5fe212
grsrujan <srujansid@gmail.com>
2018-09-18 16:32:55 -0400

    Update find_and_replace.sh

--- shell/find_and_replace.sh
+++ shell/find_and_replace.sh
@@ -1,11 +1,11 @@
 # using sed 
 # find and replace on first occurrence
 
-sed -i 's/oldtext/newtext/' file_name
+sed -i 's/oldtext/newtext/' $1
 
 # find and replace on all occurrences
 
-sed -i 's/oldtext/newtext/g' file_name
+sed -i 's/oldtext/newtext/g' $1
 
 # sed=stream editor
 # -i=inplace (save back to same input file)
