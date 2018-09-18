911bd1a907e0fbcc1ca21caa0fc0e7bd6fcfa778
grsrujan <srujansid@gmail.com>
2018-09-18 16:23:34 -0400

    Create find_and_replace.sh

---
+++ shell/find_and_replace.sh
@@ -0,0 +1,9 @@
+
+# using sed 
+
+sed -i 's/oldtext/newtext/g' file_name
+
+# sed=stream editor
+# -i=inplace (save back to same input file)
+# s=substitute command
+# g=global -replace all occurences
